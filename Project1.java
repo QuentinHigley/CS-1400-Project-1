d6980448fa5d27003ef3d7a7366d8b39bc28ca36
Quentin Higley <QuentinHigley@gmail.com>
2017-07-13 12:15:40 -0600

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.pkg1;
+
+/**
+ *
+ * @author theCh
+ */
+public class Project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String firstLine = "My name is Quentin Higley";
+        String secondLine = "My favourite food is Italian";
+        System.out.println(firstLine);
+        System.out.println(secondLine);
+    }
+    
+}
